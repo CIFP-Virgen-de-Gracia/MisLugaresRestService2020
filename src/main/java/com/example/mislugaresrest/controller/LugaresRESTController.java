a7ab64c76eb27eb5a32d97048485326405bdd412
joseluisgs <joseluisgonsan@gmail.com>
2019-12-27 11:49:45 +0100

    Listar por ID

--- src/main/java/com/example/mislugaresrest/controller/LugaresRESTController.java
+++ src/main/java/com/example/mislugaresrest/controller/LugaresRESTController.java
@@ -5,11 +5,13 @@ import com.example.mislugaresrest.dao.LugaresDAO;
 import com.example.mislugaresrest.model.Lugar;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController         // Definimos la clase como controlador REST
 @RequestMapping("/")    // Definimos la url o entrada de la API REST, este caso la raíz: localhost:8080/
@@ -45,4 +47,24 @@ public class LugaresRESTController {
         // Devolvemos la ista de productos
         return ResponseEntity.ok(l);
     }
+
+    /**
+     * Devuelve un lugar dado su ID protocolo GET
+     * GET: http://localhost:8080/lugares/{id}
+     *
+     * @param id ID del lugar
+     * @return Lugar
+     */
+    @RequestMapping(value = "lugares/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Lugar> findById(@PathVariable("id") Long id) {
+        // Buscamos el lugar por id
+        Optional<Lugar> ol = ld.findById(id);
+        // Devolvemos ellugar si existe.
+        if (ol.isPresent()) {
+            return ResponseEntity.ok(ol.get());
+        } else {
+            return ResponseEntity.noContent().build();
+        }
+    }
+
 }
