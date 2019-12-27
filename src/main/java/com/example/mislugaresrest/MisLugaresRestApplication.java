ae4d83c13218b04d13d7657c6493dc1c20a75837
joseluisgs <joseluisgonsan@gmail.com>
2019-12-27 10:37:32 +0100

    Initial commit

---
+++ src/main/java/com/example/mislugaresrest/MisLugaresRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.mislugaresrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisLugaresRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MisLugaresRestApplication.class, args);
+	}
+
+}
